d46f9664d6b014eb30e0cb3d614cdf349f97a8ad
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-21 23:45:15 +0300

    Create updater.sh

---
+++ updater.sh
@@ -0,0 +1,11 @@
+docker exec -it joins rm fas.py
+docker exec -it joins rm bond.py
+wget -O fas.py https://raw.githubusercontent.com/Lordsniffer22/force/main/fas.py &>/dev/null
+wget -O bond.py https://raw.githubusercontent.com/Lordsniffer22/force/main/bond.py &>/dev/null
+
+docker cp fas.py joins:/app
+docker cp bond.py joins:/app
+
+sudo rm -rf *.py
+
+docker restart joins
