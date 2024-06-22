ca12b577b4b027ada519077415056bdc2eb20530
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-23 01:31:28 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker exec -it joins rm fas.py
 docker exec -it joins rm bond.py
+docker exec -it joins rm keybds.py
 wget -O fas.py https://raw.githubusercontent.com/Lordsniffer22/force/main/fas.py &>/dev/null
 wget -O bond.py https://raw.githubusercontent.com/Lordsniffer22/force/main/bond.py &>/dev/null
 wget -O keybds.py https://raw.githubusercontent.com/Lordsniffer22/force/main/keybds.py &>/dev/null
